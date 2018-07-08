44fb1da36f3b621b405080efddc84c1f69304d63
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-08 22:45:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def now_serving(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else 
-    puts "Currently serving #{name}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
