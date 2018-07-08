8cdc005815c57c3fbc508e42fc5550c710cf97a0
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-08 22:51:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else 
-    katz_deli.each_with_index do 
+    katz_deli.each_with_index do |index, name| 
   end
 end
 
