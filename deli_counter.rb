fd2f89647a8d781e68869483aa89d9e0ef52b36f
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-08 22:40:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.count == log10
+  if katz_deli.count == 0
     puts ""
   puts "Currently serving #{name}."
 end
